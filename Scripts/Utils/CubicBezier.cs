3e49d553349cc765a3379ac349706ea2ee86e622
thicctapeman-dev <thicctapeman@gmail.com>
2025-03-11 18:09:12 +0100

    List Cubic

--- Scripts/Utils/CubicBezier.cs
+++ Scripts/Utils/CubicBezier.cs
@@ -14,38 +14,52 @@ namespace ThiccTapeman.Bezier
 
         public Vector3 CalculateCubicBezierPoint(float t)
         {
+            Vector3 p = CalculateCubicBezierPoint(this, t);
+
+            return p;
+        }
+
+        private static Vector3 CalculateCubicBezierPoint(CubicBezier cubicBezier, float t)
+        {
             float u = 1 - t;
             float tt = t * t;
             float uu = u * u;
             float uuu = uu * u;
             float ttt = tt * t;
 
-            Vector3 p = uuu * pointA;
-            p += 3 * uu * t * handleA;
-            p += 3 * u * tt * handleB;
-            p += ttt * pointB;
+            Vector3 p = uuu * cubicBezier.pointA;
+            p += 3 * uu * t * cubicBezier.handleA;
+            p += 3 * u * tt * cubicBezier.handleB;
+            p += ttt * cubicBezier.pointB;
 
             return p;
         }
 
         public Vector3 CalculateCubicBezierPointNormalOffset(float t, float offset)
         {
-            Vector3 point = CalculateCubicBezierPoint(t);
-            Vector3 normalOffset = CalculateCubicBezierTangent(t) * offset;
+            Vector3 offsetPosition = CalculateCubicBezierPointNormalOffset(this, t, offset);
+
+            return offsetPosition;
+        }
+
+        private static Vector3 CalculateCubicBezierPointNormalOffset(CubicBezier cubicBezier, float t, float offset)
+        {
+            Vector3 point = CalculateCubicBezierPoint(cubicBezier, t);
+            Vector3 normalOffset = CalculateCubicBezierTangent(cubicBezier, t) * offset;
 
             return point + normalOffset;
         }
 
-        private Vector3 CalculateCubicBezierTangent(float t)
+        private static Vector3 CalculateCubicBezierTangent(CubicBezier cubicBezier, float t)
         {
             float u = 1 - t;
             float tt = t * t;
             float uu = u * u;
 
-            Vector3 tangent = -3 * uu * pointA;
-            tangent += 3 * (uu - 2 * u * t) * handleA;
-            tangent += 3 * (t * (2 * u - t)) * handleB;
-            tangent += 3 * tt * pointB;
+            Vector3 tangent = -3 * uu * cubicBezier.pointA;
+            tangent += 3 * (uu - 2 * u * t) * cubicBezier.handleA;
+            tangent += 3 * (t * (2 * u - t)) * cubicBezier.handleB;
+            tangent += 3 * tt * cubicBezier.pointB;
 
             return tangent;
         }
@@ -64,6 +78,20 @@ namespace ThiccTapeman.Bezier
                 Gizmos.DrawLine(lastPoint, point);
                 lastPoint = point;
             }
+        }
+
+        public static Vector3 CalculateCubicBezierListPoint(List<CubicBezier> list, float t)
+        {
+            int index = Mathf.FloorToInt(t);
+
+            return CalculateCubicBezierPoint(list[index], t - index);
+        }
+
+        public static Vector3 CalculateCubicBezierListPointNormalOffset(List<CubicBezier> list, float t, float offset)
+        {
+            int index = Mathf.FloorToInt(t);
+
+            return CalculateCubicBezierPointNormalOffset(list[index], t - index, offset);
         }
     }
 }
